226585be79815e79105731dbc066b93a9ed169af
Felipe Luna <felipinho.luna@gmail.com>
2025-02-12 20:25:34 -0300

    feat(PostCard):add component with template, styles, and tests; update test command in package.json

---
+++ src/app/post-card/post-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Post } from '../models/post';
+
+@Component({
+  selector: 'app-post-card',
+  imports: [],
+  templateUrl: './post-card.component.html',
+  styleUrl: './post-card.component.css'
+})
+export class PostCardComponent {
+
+  readonly post = input.required<Post>();
+
+}
