48e5e06ed587d6a747ca709bc1d8e0ef0c4e5afd
Felipe Luna <felipinho.luna@gmail.com>
2025-02-13 15:45:57 -0300

    test(AppComponent): enhance unit tests to verify post addition functionality

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+      expect(component).toBeTruthy();
+  });
+
+  it('should add a post to list', () => {
+    const post = { title: 'Post title Luna', body: 'Post body Luna' };
+    component.addPost(post);
+    expect(component.posts()).toEqual([{ title: 'Post title Luna', body: 'Post body Luna' }]);
+
+    component.addPost({title: 'Post title Luna 2', body: 'Post body Luna 2'});
+    expect(component.posts()).toEqual([{ title: 'Post title Luna', body: 'Post body Luna' },
+                              {title: 'Post title Luna 2', body: 'Post body Luna 2'}]);
   });
 
 });
