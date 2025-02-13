aa4fc83d3270e4b064ca0a820725a983c9541dee
Felipe Luna <felipinho.luna@gmail.com>
2025-02-13 12:37:39 -0300

    feat(App): replace post form section with PostForm component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PostFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
